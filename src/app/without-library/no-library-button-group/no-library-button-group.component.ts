754fb28c89f9cfd2474bbd969fd8371894eaf6d9
Alexander Skoropad <alex@skoropad.dev>
2022-11-15 12:55:37 +0100

    dev

--- src/app/without-library/no-library-button-group/no-library-button-group.component.ts
+++ src/app/without-library/no-library-button-group/no-library-button-group.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, ChangeDetectionStrategy, Component, ContentChildren, OnInit, QueryList} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {NoLibraryButtonComponent} from '../no-library-button/no-library-button.component';
+import {startWith, switchMap} from 'rxjs/operators';
+import {merge} from 'rxjs';
 
 @Component({
   selector: 'app-no-library-button-group',
   templateUrl: './no-library-button-group.component.html',
-  styleUrls: ['./no-library-button-group.component.css']
+  styleUrls: ['./no-library-button-group.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NoLibraryButtonGroupComponent
+  }]
 })
-export class NoLibraryButtonGroupComponent implements OnInit {
+export class NoLibraryButtonGroupComponent<T> implements OnInit, AfterContentInit, ControlValueAccessor {
+  @ContentChildren(NoLibraryButtonComponent)
+  buttons: QueryList<NoLibraryButtonComponent<T>>;
+
+  private onChange: (v: T) => void;
+  private onTouched: () => void;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  ngAfterContentInit(): void {
+    this.buttons
+        .changes
+        .pipe(
+            startWith(this.buttons),
+            switchMap((buttons: QueryList<NoLibraryButtonComponent<T>>) =>
+              merge(...buttons.map((b: NoLibraryButtonComponent<T>) => b.onSelected()))
+            )
+        )
+        .subscribe((selected: NoLibraryButtonComponent<T>) => {
+          this.selectValue(selected.value);
+          this.onChange(selected.value);
+        })
+  }
+
+  registerOnChange(fn: (v: T) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void) {
+    this.onTouched = fn;
+  }
+
+  writeValue(obj: T): void {
+    this.selectValue(obj);
+  }
+
+  private selectValue(v: T): void {
+    this.buttons.forEach((b: NoLibraryButtonComponent<T>) => {
+      b.value === v ? b.select() : b.deselect();
+    })
+  }
 }
--- src/app/without-library/no-library-button/no-library-button.component.ts
+++ src/app/without-library/no-library-button/no-library-button.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit
+} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {mapTo} from 'rxjs/operators';
 
 @Component({
   selector: 'app-no-library-button',
   templateUrl: './no-library-button.component.html',
-  styleUrls: ['./no-library-button.component.css']
+  styleUrls: ['./no-library-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NoLibraryButtonComponent implements OnInit {
+export class NoLibraryButtonComponent<T> {
+  @Input()
+  value: T | null = null;
 
-  constructor() { }
+  @HostBinding('attr.data-checked')
+  selected: boolean = false;
 
-  ngOnInit(): void {
+  private selected$: Subject<void> = new Subject<void>();
+
+  constructor(
+      private readonly changeDetectorRef: ChangeDetectorRef,
+  ) {
   }
 
+  onSelected(): Observable<NoLibraryButtonComponent<T>> {
+    return this.selected$.asObservable().pipe(mapTo(this));
+  }
+
+  @HostListener('click')
+  clickEvent(): void {
+    this.selected$.next();
+  }
+
+  select(): void {
+    this.selected = true;
+    this.changeDetectorRef.markForCheck();
+  }
+
+  deselect(): void {
+    this.selected = false;
+    this.changeDetectorRef.markForCheck();
+  }
 }
